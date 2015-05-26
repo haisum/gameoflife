973155c0242aa8dc3e32069f4ba77b3581bfec7c
haisum <haisumbhatti@gmail.com>
2015-05-26 23:12:48 +0500

    Adding overview for package, documentation for functions and data types. More to come as we continue development

--- draw.go
+++ draw.go
@@ -1,14 +1,29 @@
+// Package gameoflife is implementation of Game of Life game by John Conway
+//
+// Installation
+//
+// go get github.com/haisum/gameoflife
+// go run src/github.com/haisum/gameoflife/_simulator/terminal.go
+//
+// Usage
+//
+// Compile files in _simulator folder
+// Enjoy yourself smooth simulation
 package gameoflife
 
 import (
 	"fmt"
 	"github.com/mgutz/ansi"
+	"runtime"
 	"strings"
 )
 
-var normal = ansi.ColorFunc("green+h:black")
-var highlight = ansi.ColorFunc("black+B:green")
+// bash ansi color that we are going to use for displaying dead cells
+var dead = ansi.ColorFunc("black+B:green")
+var alive = ansi.ColorFunc("177+b:18")
 
+// This function draws a x X y grid on terminal
+// and highlights points at location defined by Point objects inside activePoints array
 func Draw(x int, y int, activePoints []Point) {
 	fmt.Println(strings.Repeat(" -", x))
 	for j := 0; j < y; j++ {
@@ -19,11 +34,8 @@ func Draw(x int, y int, activePoints []Point) {
 				fmt.Print("|")
 			}
 
-			if point.InSlice(activePoints) {
-				fmt.Printf(highlight(" "))
-			} else {
-				fmt.Printf(normal(" "))
-			}
+			printCell(point.InSlice(activePoints))
+
 			fmt.Printf("|")
 			if i == x-1 {
 				fmt.Println()
@@ -34,6 +46,25 @@ func Draw(x int, y int, activePoints []Point) {
 	fmt.Printf(ansi.ColorCode("reset"))
 }
 
+// Prints a cell of a grid on terminal
+func printCell(isAlive bool) {
+	if runtime.GOOS == "windows" {
+		if isAlive {
+			fmt.Printf("O")
+		} else {
+			fmt.Printf(" ")
+		}
+	} else {
+		if isAlive {
+			fmt.Printf(alive(" "))
+		} else {
+			fmt.Printf(dead(" "))
+		}
+	}
+}
+
+// Clears current console
+// Equivalent to clscr or clear
 func Clear() {
 	fmt.Printf("\033[H\033[2J")
 }
--- point.go
+++ point.go
@@ -1,10 +1,12 @@
 package gameoflife
 
+// Point struct represents a single cell at location x,y of the life grid
 type Point struct {
 	X int
 	Y int
 }
 
+// Checks if a particular Point object is in supplied Point array
 func (a *Point) InSlice(points []Point) bool {
 	for _, b := range points {
 		if b.X == a.X && b.Y == a.Y {
